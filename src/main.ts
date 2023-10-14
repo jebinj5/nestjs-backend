a79e670452a29e18ce5637dc6414d7fdbb71515c
beyondnxt <contact@beyondnxt.com>
2023-10-14 12:09:31 +0530

    Enable CROS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(PORT);
 }
 bootstrap();
